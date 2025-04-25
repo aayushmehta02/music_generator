c0b19bc68b8691971a8ef10fabfba61ae13652d6
himanshimodi <modihimanshi04@gmail.com>
2025-04-25 12:54:16 +0530

    changes in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react' // Importing StrictMode for highlighting potential problems in an application
+import { createRoot } from 'react-dom/client' // Importing createRoot from React DOM for concurrent rendering
+import './index.css' // Importing global CSS styles
+import App from './App.jsx' // Importing the main App component
 
+// Creating the root of the React application and rendering the App inside StrictMode
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> {/* Rendering the App component inside StrictMode for development checks */}
   </StrictMode>,
 )
